67f741827ed4c16307a0db20f4183dd69c19b5a6
Luverduck <hyekko92@gmail.com>
2025-08-29 22:50:04 +0900

    04.05. Node.js 라이브러리 사용

--- one-bite-react/section04/src/index.js
+++ one-bite-react/section04/src/index.js
@@ -1,7 +1,7 @@
-console.log("안녕 Node.js");
+// console.log("안녕 Node.js");
 
-// CommonJS 모듈 시스템 방식
 /*
+// CommonJS 모듈 시스템 방식
 const moduleData = require("./math");
 console.log(moduleData);
 console.log(moduleData.add(1, 2));
@@ -12,15 +12,22 @@ console.log(add(1, 2));
 console.log(subtract(1, 3));
 */
 
-// ES 모듈 시스템 방식
 /*
+// ES 모듈 시스템 방식
 import { add, subtract } from "./math.js";
 console.log(add(1, 2));
 console.log(subtract(1, 3));
 */
 
+/*
 // ES 모듈 시스템의 export default
 import multiply, { add, subtract } from "./math.js";
 console.log(add(1, 2));
 console.log(subtract(1, 3));
 console.log(multiply(2, 3));
+*/
+
+// npm을 통해 다운로드한 라이브러리 사용
+import randomColor from "randomcolor";
+const color = randomColor();
+console.log(color);
