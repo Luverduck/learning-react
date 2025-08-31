dfaae793ce12a342549f2f068bbfafeee928f6c3
Luverduck <hyekko92@gmail.com>
2025-09-01 02:17:52 +0900

    06.05. 이벤트 핸들링

--- one-bite-react/section06/src/components/Button.jsx
+++ one-bite-react/section06/src/components/Button.jsx
@@ -1,7 +1,15 @@
 const Button = ({ text, color="black", children }) => {
+  const onClickEvent = (e) => {
+    console.log(text);
+    console.log(e);
+  };
+
   return (
     <>
-      <button style={{ color: color }}>
+      <button 
+        onClick={onClickEvent}
+        style={{ color: color }}
+      >
         {text} - {color.toUpperCase()}
         {children}
       </button>
