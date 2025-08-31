4b776bc7bf2e7b88caa272ca173971f368ddd973
Luverduck <hyekko92@gmail.com>
2025-08-31 16:25:33 +0900

    06.01. 실습 준비하기

---
+++ one-bite-react/section06/eslint.config.js
@@ -0,0 +1,33 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      // 사용되지 않는 변수가 있을 때 경고, 오류 비활성화
+      'no-unused-vars': 'off',
+      // props에 대한 propTypes를 정의하지 않았을 때 경고 비활성화
+      'react/prop-types': 'off'
+    },
+  },
+])
