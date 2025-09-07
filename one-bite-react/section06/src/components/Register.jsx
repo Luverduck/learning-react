5c1b722aa7bdd8fe801b6a520b3b19081af2e2c9
Luverduck <hyekko92@gmail.com>
2025-09-07 21:44:32 +0900

    06.10. ref - 컴포넌트의 변수 생성하기

--- one-bite-react/section06/src/components/Register.jsx
+++ one-bite-react/section06/src/components/Register.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 const Register = () => {
 
@@ -9,17 +9,29 @@ const Register = () => {
     biography: '',
   });
 
+  const countRef = useRef(0);
+  const inputRef = useRef();
+
   const onChange = (e) => {
+    countRef.current++;
+    console.log(countRef.current);
     setInput({
       ...input,
       [e.target.name]: e.target.value,
     })
-  }
+  };
+
+  const onSubmit = () => {
+    if(input.name === '') {
+      inputRef.current.focus();
+    }
+  };
 
   return (
     <div>
       <div>
         <input 
+          ref={inputRef}
           name='name'
           value={input.name}
           onChange={onChange} 
@@ -53,6 +65,7 @@ const Register = () => {
           onChange={onChange}
         />
       </div>
+      <button onClick={onSubmit}>제출</button>
     </div>
   );
 }
