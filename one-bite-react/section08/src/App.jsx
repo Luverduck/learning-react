9a2d36cff2f0eee9b689794d67187650d5f8d872
Luverduck <hyekko92@gmail.com>
2025-09-11 00:04:11 +0900

    08.02. useEffect 사용하기

--- one-bite-react/section08/src/App.jsx
+++ one-bite-react/section08/src/App.jsx
@@ -1,18 +1,31 @@
 import './App.css';
 import Viewer from './components/Viewer';
 import Controller from './components/Controller';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-  // 현재 카운트
   const [count, setCount] = useState(0);
-  // 현재 카운트에 대한 set 함수
+  const [input, setInput] = useState('');
+  
+  // useEffect(() => {}, [])
+  useEffect(()=> {
+    console.log(`count: ${count} / input: ${input}`);
+  }, [count, input]);
+
   const onClickButton = (value) => {
     setCount(count + value);
+    // 비동기로 setCount 함수가 실행되므로 변경 전 count 값 출력
+    // console.log(`count: ${count}`);
   }
+  
   return (
     <div className="App">
       <h1>Simple Counter</h1>
+      <section>
+        <input value={input} onChange={(e) => {
+          setInput(e.target.value);
+        }} />
+      </section>
       <section>
         <Viewer count={count} />
       </section>
