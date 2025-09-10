444045f99fdab682b0c95b1ae07c70b47cc74838
Luverduck <hyekko92@gmail.com>
2025-09-10 12:34:23 +0900

    07.03. 기능 구현하기

--- one-bite-react/section07/src/App.jsx
+++ one-bite-react/section07/src/App.jsx
@@ -1,19 +1,23 @@
-import { useState } from 'react';
 import './App.css';
 import Viewer from './components/Viewer';
 import Controller from './components/Controller';
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // 현재 카운트
+  const [count, setCount] = useState(0);
+  // 현재 카운트에 대한 set 함수
+  const onClickButton = (value) => {
+    setCount(count + value);
+  }
   return (
     <div className="App">
       <h1>Simple Counter</h1>
       <section>
-        <Viewer />
+        <Viewer count={count} />
       </section>
       <section>
-        <Controller />
+        <Controller onClickButton={onClickButton} />
       </section>
     </div>
   )
