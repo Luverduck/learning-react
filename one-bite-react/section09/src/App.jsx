d635f475c846c0dc75a0fc812244a0ac76a3552d
Luverduck <hyekko92@gmail.com>
2025-09-14 19:04:42 +0900

    09.04. Create - TO-DO 추가하기

--- one-bite-react/section09/src/App.jsx
+++ one-bite-react/section09/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Header from './components/Header';
 import Editor from './components/Editor';
 import List from './components/List';
@@ -28,11 +28,25 @@ const mockData = [
 function App() {
 
   const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(0); // ID 설정을 위한 ref
+
+  // 새로운 TO-DO를 생성하는 함수
+  const onCreate = (content) => {
+    // 새로운 TO-DO 생성
+    const newTodo = {
+      id: idRef.current++, // 새로운 TO-DO를 생성할 때마다 ID 증가
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    }
+    // 새로운 TO-DO와 기존 todos의 요소를 합친 배열을 새로운 todos로 설정
+    setTodos([newTodo, ...todos]);
+  };
 
   return (
     <div className='App'>
       <Header />
-      <Editor />
+      <Editor onCreate={onCreate} />
       <List />
     </div>
   )
