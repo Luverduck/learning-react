fcd315964e953618fd374594c9c75fcfcaed4c7b
Luverduck <hyekko92@gmail.com>
2025-09-15 21:11:27 +0900

    Section10 초기 커밋

--- one-bite-react/section10/src/App.jsx
+++ one-bite-react/section10/src/App.jsx
@@ -28,25 +28,21 @@ const mockData = [
 function App() {
 
   const [todos, setTodos] = useState(mockData);
-  const idRef = useRef(0); // ID 설정을 위한 ref
+  const idRef = useRef(0);
 
-  // 새로운 TO-DO를 생성하는 함수
   const onCreate = (content) => {
-    // 새로운 TO-DO 생성
     const newTodo = {
-      id: idRef.current++, // 새로운 TO-DO를 생성할 때마다 ID 증가
+      id: idRef.current++,
       isDone: false,
       content: content,
       date: new Date().getTime(),
     }
-    // 새로운 TO-DO와 기존 todos의 요소를 합친 배열을 새로운 todos로 설정
     setTodos([newTodo, ...todos]);
   };
 
-  // TO-DO의 체크 상태를 변경하는 함수
   const onUpdate = (targetId) => {
     setTodos(todos.map((todo) => {
-      if(todo.id === targetId) {
+      if (todo.id === targetId) {
         return {
           ...todo,
           isDone: !todo.isDone,
@@ -56,11 +52,9 @@ function App() {
     }));
   };
 
-  // TO-DO 리스트에서 TO-DO를 제거하는 함수
   const onDelete = (targetId) => {
     setTodos(todos.filter((todo) => {
-      // 삭제 대상이 아닌 요소만 필터링
-      return todo.id !== targetId; 
+      return todo.id !== targetId;
     }));
   };
 
--- one-bite-react/section10/src/components/TodoItem.jsx
+++ one-bite-react/section10/src/components/TodoItem.jsx
@@ -2,12 +2,10 @@ import './TodoItem.css';
 
 const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
 
-  // 체크 박스 입력 요소의 onChange에 대한 이벤트 핸들러
   const onChangeCheckBox = () => {
     onUpdate(id)
   };
 
-  // 삭제 버튼의 onClick에 대한 이벤트 핸들러
   const onClickDeleteButton = () => {
     onDelete(id);
   };
