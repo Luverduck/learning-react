a82981583a6f97a2c19417d6206d96a655aa4ad4
Luverduck <hyekko92@gmail.com>
2025-09-21 23:44:49 +0900

    13.06. 폰트, 이미지, 레이아웃 설정하기

--- one-bite-react/section13/src/App.jsx
+++ one-bite-react/section13/src/App.jsx
@@ -5,6 +5,9 @@ import Diary from './pages/Diary';
 import New from './pages/New';
 import Notfound from './pages/NotFound';
 
+// assets 폴더의 이미지 불러오기
+import getEmotionImage from './util/get-emotion-image';
+
 function App() {
 
   // 페이지 이동 함수 반환
@@ -18,6 +21,14 @@ function App() {
   return (
     <>
       <div>
+        {/* assets 폴더에서 불러온 이미지 렌더링 */}
+        <img src={getEmotionImage(1)} />
+        <img src={getEmotionImage(2)} />
+        <img src={getEmotionImage(3)} />
+        <img src={getEmotionImage(4)} />
+        <img src={getEmotionImage(5)} />
+      </div>
+      <div>
         {/* Link 컴포넌트 정의 */}
         <Link to={'/'}>Home</Link>
         <Link to={'/new'}>New</Link>
@@ -27,7 +38,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
-        <Route path="/diary/:id/:pw" element={<Diary />} />
+        <Route path="/diary/:id" element={<Diary />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
     </>
