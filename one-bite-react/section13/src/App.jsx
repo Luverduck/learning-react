70d7821a808a5e39e7936d65e157f9288e4b356f
Luverduck <hyekko92@gmail.com>
2025-09-21 17:18:36 +0900

    13.04. 페이지 라우팅 - 페이지 이동

--- one-bite-react/section13/src/App.jsx
+++ one-bite-react/section13/src/App.jsx
@@ -1,15 +1,30 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Diary from './pages/Diary';
 import New from './pages/New';
 import Notfound from './pages/NotFound';
 
 function App() {
+
+  // 페이지 이동 함수 반환
+  const nav = useNavigate();
+  
+  // onClick 이벤트에 대한 이벤트 핸들러 정의
+  const onClickButton = () => {
+    nav('/new');
+  };
+
   return (
     <>
+      <div>
+        {/* Link 컴포넌트 정의 */}
+        <Link to={'/'}>Home</Link>
+        <Link to={'/new'}>New</Link>
+        <Link to={'/diary'}>Diary</Link>
+      </div>
+      <button onClick={onClickButton}>New 페이지로 이동</button>
       <Routes>
-              <div>Hello</div>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary" element={<Diary />} />
