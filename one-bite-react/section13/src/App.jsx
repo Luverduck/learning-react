8bc9d7369be21c37ff2fb35ebfbdf4faad5cc76a
Luverduck <hyekko92@gmail.com>
2025-09-24 00:22:30 +0900

    13.08. 일기 관리 기능 구현하기 1

--- one-bite-react/section13/src/App.jsx
+++ one-bite-react/section13/src/App.jsx
@@ -1,50 +1,40 @@
 import './App.css';
-import { Routes, Route, Link, useNavigate } from 'react-router-dom';
+import { useReducer } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Diary from './pages/Diary';
 import New from './pages/New';
 import Edit from './pages/Edit';
 import Notfound from './pages/NotFound';
-import Button from './components/Button';
-import Header from './components/Header';
 
-// assets 폴더의 이미지 불러오기
-import getEmotionImage from './util/get-emotion-image';
+// 초기 데이터
+const mockData = [
+  {
+    id: 1, 
+    createDate: new Date().getTime(), 
+    emotionId: 1, 
+    content: '1번 일기 내용',
+  },
+  {
+    id: 2, 
+    createDate: new Date().getTime(), 
+    emotionId: 2, 
+    content: '2번 일기 내용',
+  },
+]
+
+// reducer 함수
+function reducer(state, action) {
+  return state;
+};
 
 function App() {
 
-  // 페이지 이동 함수 반환
-  const nav = useNavigate();
-
-  // onClick 이벤트에 대한 이벤트 핸들러 정의
-  const onClickButton = () => {
-    nav('/new');
-  };
+  // useReducer를 통해 일기 리스트를 State로 정의
+  const [data, dispach] = useReducer(reducer, mockData);
 
   return (
     <>
-      {/* Header 컴포넌트 렌더링 */}
-      <Header
-        title={'Header'}
-        leftChild={<Button text={'Left'} />}
-        rightChild={<Button text={'Right'} />}
-      />
-      {/* Button 컴포넌트 렌더링 */}
-      <Button
-        text={'일반'}
-        onClick={() => { console.log("일반버튼 클릭") }}
-      />
-      <Button
-        text={'긍정'}
-        type={'POSITIVE'}
-        onClick={() => { console.log("긍정버튼 클릭") }}
-      />
-      <Button
-        text={'부정'}
-        type={'NEGATIVE'}
-        onClick={() => { console.log("부정버튼 클릭") }}
-      />
-
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/new' element={<New />} />
