5da545e4edffddc63907456fcd238c0c76b6d52e
Luverduck <hyekko92@gmail.com>
2025-09-28 16:07:46 +0900

    이미지 불러오기 모듈 export 변경

--- one-bite-react/section13/src/util/get-emotion-image.js
+++ one-bite-react/section13/src/util/get-emotion-image.js
@@ -4,7 +4,7 @@ import emotion3 from '../assets/emotion3.png';
 import emotion4 from '../assets/emotion4.png';
 import emotion5 from '../assets/emotion5.png';
 
-const getEmotionImage = (emotionId) => {
+export const getEmotionImage = (emotionId) => {
   switch(emotionId) {
     case 1: return emotion1; break;
     case 2: return emotion2; break;
@@ -14,5 +14,3 @@ const getEmotionImage = (emotionId) => {
     default: return null; break;
   }
 };
-
-export default getEmotionImage;
