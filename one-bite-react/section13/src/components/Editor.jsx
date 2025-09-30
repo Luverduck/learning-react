8e591e3b5e8a474f6c4bad46bfb694de9b150d4d
Luverduck <hyekko92@gmail.com>
2025-09-30 20:43:28 +0900

    New 페이지에서 오늘의 감정 이미지가 렌더링되지 않는 문제 수정

--- one-bite-react/section13/src/components/Editor.jsx
+++ one-bite-react/section13/src/components/Editor.jsx
@@ -26,9 +26,7 @@ const Editor = () => {
       <section className='emotion_section'>
         <h4>오늘의 감정</h4>
         <div className='emotion_list_wrapper'>
-          {emotionList.map((item) => {
-            <EmotionItem key={item.emotionId} {...item} isSelected={item.emotionId === emotionId} />
-          })}
+          {emotionList.map((item) => <EmotionItem key={item.emotionId} {...item} isSelected={item.emotionId === emotionId} />)}
         </div>
       </section>
       {/** 오늘의 일기 영역 */}
